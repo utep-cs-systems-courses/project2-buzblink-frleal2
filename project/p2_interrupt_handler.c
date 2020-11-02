4dd3938bca71115767306e3a7341be8661c56b16
frleal2 <45012732+frleal2@users.noreply.github.com>
2020-11-01 18:28:14 -0700

    Create p2_interrupt_handler.c

---
+++ project/p2_interrupt_handler.c
@@ -0,0 +1,11 @@
+#include <msp430.h>
+#include "switches.h"
+
+/* Switch on P2 (S1,S2,S3,S4) */
+void
+__interrupt_vec(PORT2_VECTOR) Port_2(){
+  if (P2IFG & SWITCHES) {	      
+    P2IFG &= ~SWITCHES;		      /* clear pending sw interrupts */
+    switch_interrupt_handler();	/* single handler for all switches */
+  }
+}
